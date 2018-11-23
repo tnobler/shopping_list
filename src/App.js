30cfd45a1eff8caeb01a50ba34d51a044945a8d1
Tanner Noble <tannernoble@gmail.com>
2018-11-23 00:13:35 -0600

    Added redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -10,10 +13,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList/>
+        </div>
+      </Provider>
     );
   }
 }
